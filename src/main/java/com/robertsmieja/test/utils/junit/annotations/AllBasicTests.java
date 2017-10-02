8c1c777026a4ecce7bf134e6bbc6e1660066b579
Robert Smieja <robertsmieja@gmail.com>
2017-10-02 02:01:31 -0400

    Add start of alternate Annotation usage

---
+++ src/main/java/com/robertsmieja/test/utils/junit/annotations/AllBasicTests.java
@@ -0,0 +1,15 @@
+package com.robertsmieja.test.utils.junit.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@EqualsTests
+@GettersAndSettersTests
+@HashCodeTests
+@ToStringTests
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE})
+public @interface AllBasicTests {
+}
